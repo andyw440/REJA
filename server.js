5e6c1d08892499e48f74b1cfbd71315f15fc4cee
andyw440 <nozimovasror47@gmail.com>
2025-09-19 01:00:37 +0900

    author routeda portfolio yasaldi.

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ console.log('web serverni boshlash .');
 const express = require('express')
 const app = express()
 const http = require("http")
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json" , "utf8" , (err,data) =>{
+    if(err){
+        console.log('ERROR',err);
+    }else{
+        user = JSON.parse(data)
+    }
+})
 
 // #1 Kirish code
 
-app.use(express.static('public'))
+app.use(express.static('public')) // Middleware design pattern
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
@@ -24,6 +33,10 @@ app.set("view engine" , "ejs")
 app.post("/create-item" , function(req,res){
     console.log(req.body)
     res.json({test:"success"})
+})
+
+app.get("/author", (req,res) => {
+    res.render("author", { user: user })
 })
 
 app.get("/" , function(req,res){
