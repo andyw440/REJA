de07f747e2411263f1b162e12a0c23a24ec52fad
andyw440 <nozimovasror47@gmail.com>
2025-09-20 19:06:19 +0900

    train.js da countLetter function yaratildi .

--- train.js
+++ train.js
@@ -1,1 +1,22 @@
 console.log('training task ishga tushdi.');
+
+/*
+    Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi 
+    sozdan qatnashga sonini return qilishi kerak boladi. MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+function countLetter(letter,word){
+    const arr = word.split('')
+    let count = 0
+   
+    arr.forEach(item => {
+        if(item === letter){
+            count++
+        }
+    })
+    return count
+
+    
+}
+
+console.log(countLetter('r' , 'cristiano'));
+
