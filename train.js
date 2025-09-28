ebd301300988caf6b0708495fcbdd5e599663fda
andyw440 <nozimovasror47@gmail.com>
2025-09-28 18:38:35 +0900

    MITASK-C done

--- train.js
+++ train.js
@@ -56,21 +56,97 @@
 
 
 // TASK B
-function countNumber(text){
-    if(typeof text === 'string'){
-        text =  text.split('')
-        let counter = 0
-        text.forEach(item => {
-            if(!isNaN(item) && item !== ' '){
-                counter++
-            }
-        })
-        return counter
+// function countNumber(text){
+//     if(typeof text === 'string'){
+//         text =  text.split('')
+//         let counter = 0
+//         text.forEach(item => {
+//             if(!isNaN(item) && item !== ' '){
+//                 counter++
+//             }
+//         })
+//         return counter
+//     }
+
+// }
+
+// console.log('TASK B , ishtirok etgan raqamlar soni:',countNumber('salom1232'));
+
+
+console.log('================= TRAIN MODE =================');
+
+/* 
+    MITASK-C 
+
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+@MITASK
+*/
+
+
+class Shop{
+    constructor(countOfNon, countOfLagmon, CountOfCola ){
+        this.countOfNon = countOfNon
+        this.countOfLagmon = countOfLagmon
+        this.CountOfCola = CountOfCola
     }
 
+    // Methods
+
+    qoldiq(){
+        const time = new Date()
+        const hours = time.getHours()
+        const minutes = time.getMinutes()
+        return `hozir ${hours}:${minutes}da ${this.countOfNon}ta non , ${this.countOfLagmon}ta lagmon , ${this.CountOfCola}ta cola mavjud!`
+    }
+
+    sotish(sellItem, sellCount){
+        if(sellItem === 'non'){
+            this.countOfNon -= sellCount
+        }
+        if(sellItem === 'lagmon'){
+            this.countOfLagmon -= sellCount
+        }
+
+        if(sellItem === 'cola'){
+            this.CountOfCola -= sellCount
+        }
+    }
+
+    qabul(receiveItem , receiveCount){
+        if(receiveItem === 'non'){
+            this.countOfNon +=receiveCount
+        }
+        if(receiveItem === 'lagmon'){
+            this.countOfLagmon += receiveCount
+        }
+        if(receiveItem === 'cola'){
+            this.CountOfCola += receiveCount
+        }
+    }
 }
 
-console.log('TASK B , ishtirok etgan raqamlar soni:',countNumber('salom1232'));
+const shop = new Shop(4, 5, 2)
+
+console.log(shop.qoldiq());
+console.log('========================');
+
+shop.sotish('non',2)
+
+console.log(shop.qoldiq());
+
+console.log('========================');
+
+shop.qabul('cola',5)
+console.log(shop.qoldiq());
+
+
+
+
+
+
+
 
 
 
