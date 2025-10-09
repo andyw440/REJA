c793180afb300d67022f24e968187ae0b5ae2584
andyw440 <nozimovasror47@gmail.com>
2025-10-09 23:22:51 +0900

    MITASK-G done.

--- train.js
+++ train.js
@@ -205,22 +205,43 @@ MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
 
         */
 
-            function findDoublers(string){
-                let count = {}
-                for(let i=0; i < string.length; i++){
-                    let char = string[i]
+            // function findDoublers(string){
+            //     let count = {}
+            //     for(let i=0; i < string.length; i++){
+            //         let char = string[i]
 
-                    if(count[char]){                        
-                        return true
-                    }else{
-                        count[char] = 1
-                    }
+            //         if(count[char]){                        
+            //             return true
+            //         }else{
+            //             count[char] = 1
+            //         }
                     
-                }
-                return false
+            //     }
+            //     return false
+            // }
+
+            // console.log(findDoublers('apple'));
+            
+
+            /* 
+                TASK G:
+
+                    Yagona parametrga ega function tuzing.
+                    Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+                    Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+                    MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+                    Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+                    Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+            */
+
+            function getHighestIndex(array){
+                 let highestNumber = array.reduce((a,b) => (a > b ? a:b))
+                return array.indexOf(highestNumber)
+            
             }
 
-            console.log(findDoublers('apple'));
+            console.log(getHighestIndex([5,2,8,6]));
             
 
 /*
